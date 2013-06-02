c04699dc9da725fc262f5bf1d757e52be798386a
Thomas Eberlein <uwte@cs.washington.edu>
2013-06-02 15:47:17 -0700

    Clean Up tests
    Changed variable names, added comments.

--- MinionAppTest/src/uw/cse403/minion/test/EditGroupActivityTest.java
+++ MinionAppTest/src/uw/cse403/minion/test/EditGroupActivityTest.java
@@ -9,7 +9,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
- * 
+ * White box texts for EditGroupActivity, testing that the basic
+ * UI elements are displayed correctly. 
  * @author Thomas Eberlein
  *
  */
@@ -20,26 +21,38 @@ public class EditGroupActivityTest extends
 		super(EditGroupActivity.class);
 	}
 	
+	/**
+	 * Tests that the game master label appears with correct text. 
+	 */
 	public void testGameMasterAppears(){
-		LinearLayout gm_info = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.game_master_info);
-		TextView title = (TextView) gm_info.getChildAt(0);
+		LinearLayout gmInfo = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.game_master_info);
+		TextView title = (TextView) gmInfo.getChildAt(0);
 		assertEquals("Game Master:", title.getText());
 	}
 	
+	/**
+	 * Tests that the players label appears with the correct text.
+	 */
 	public void testPlayersTitleAppears(){
 		TextView players = (TextView) getActivity().findViewById(uw.cse403.minion.R.id.players_title);
 		assertEquals("Players", players.getText());
 	}
 	
+	/**
+	 * Tests that the invite button appears, and has the correct text. 
+	 */
 	public void testInviteButtonAppears(){
 		LinearLayout buttons = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.editing_buttons);
-		Button invite_button = (Button) buttons.getChildAt(0);
-		assertEquals("Invite User", invite_button.getText());
+		Button inviteButton = (Button) buttons.getChildAt(0);
+		assertEquals("Invite User", inviteButton.getText());
 	}
 	
+	/**
+	 * Tests that the finish button appears, and has the correct text. 
+	 */
 	public void testDoneButtonAppears(){
 		LinearLayout buttons = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.editing_buttons);
-		Button finish_button = (Button) buttons.getChildAt(1);
-		assertEquals("Finish", finish_button.getText());
+		Button finishButton = (Button) buttons.getChildAt(1);
+		assertEquals("Finish", finishButton.getText());
 	}
 }
--- MinionAppTest/src/uw/cse403/minion/test/GroupCreateActivityTest.java
+++ MinionAppTest/src/uw/cse403/minion/test/GroupCreateActivityTest.java
@@ -7,7 +7,8 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 /**
- * 
+ * White box texts for GroupCreateActivity, testing that the basic
+ * UI elements are displayed correctly. 
  * @author Thomas Eberlein
  *
  */
@@ -18,25 +19,38 @@ public class GroupCreateActivityTest extends
 		super(GroupCreateActivity.class);
 	}
 	
+	/**
+	 * Tests to make sure the proper prompt is displayed for the entering
+	 * the new group's name. 
+	 */
 	public void testGroupNameAppears(){
-		LinearLayout group_name = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.group_name);
-		TextView group_prompt = (TextView) group_name.getChildAt(0);
-		assertEquals("EnterGroupName", group_prompt.getText());
+		LinearLayout groupName = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.group_name);
+		TextView groupPrompt = (TextView) groupName.getChildAt(0);
+		assertEquals("Enter Group Name", groupPrompt.getText());
 	}
 	
+	/**
+	 * Tests to make sure correct hint is displayed for entering a group's name. 
+	 */
 	public void testEditGroupNameRightPrompt(){
-		LinearLayout group_name = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.group_name);
-		EditText group_prompt = (EditText) group_name.getChildAt(1);
-		assertEquals("Name", group_prompt.getHint());
+		LinearLayout groupName = (LinearLayout) getActivity().findViewById(uw.cse403.minion.R.id.group_name);
+		EditText groupPrompt = (EditText) groupName.getChildAt(1);
+		assertEquals("Name", groupPrompt.getHint());
 	}
 	
+	/**
+	 * Tests that add button appears, and has the correct text.
+	 */
 	public void testAddButtonAppears(){
-		Button add_button = (Button) getActivity().findViewById(uw.cse403.minion.R.id.add_user_button);
-		assertEquals("Add", add_button.getText());
+		Button addButton = (Button) getActivity().findViewById(uw.cse403.minion.R.id.add_user_button);
+		assertEquals("Add", addButton.getText());
 	}
 	
+	/**
+	 * Tests that the done button appears, and has the correct text. 
+	 */
 	public void testDoneButtonAppears(){
-		Button done_button = (Button) getActivity().findViewById(uw.cse403.minion.R.id.create_group_button);
-		assertEquals("Done", done_button.getText());
+		Button doneButton = (Button) getActivity().findViewById(uw.cse403.minion.R.id.create_group_button);
+		assertEquals("Done", doneButton.getText());
 	}
 }
